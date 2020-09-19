5f065a7da7693c605b9b36d8776984f751722655
arminromhanyi <armin.romhanyi@gmail.com>
2020-09-19 18:24:04 +0200

    improved server structure: basic error handling, config, logging

--- server/src/app/server.js
+++ server/src/app/server.js
@@ -1,32 +1,44 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import { sequelize } from '../../models';
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 
-
+const path = require('path')
 const port = 9999
 const context = require('../../models')
+const config = require(path.join(__dirname, '../../config/config.json'))
+
+const corsOptions = {
+  origin: config.origin,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 const app = express()
+const winston = require('winston')
+
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }), // easier to check the end-result
+    new winston.transports.Console({ filename: 'error.log', level: 'error' })
+  ]
+})
+
 app.listen(port,
   console.log(`Server listening on port ${port}`))
 
-// app.get('/', (req, res) => {
-//     res.send("asd")
-// });
-
-
 app.use(
-  cors(),
+  cors(corsOptions), // we dont need to enable pre-flight for this solution, as there are no delete/put methods in use
   bodyParser.urlencoded({ extended: true }),
   bodyParser.json()
 )
 
-export const saveNumber = async number => {
+const saveOperand = (number) => {
   return context.Operand.create(number)
 }
 
-export const getNumbersForCalc = async calcId => {
+const getOperandsForCalculation = (calcId) => {
   return context.Operand.findAll(
     {
       where: {
@@ -35,22 +47,61 @@ export const getNumbersForCalc = async calcId => {
     })
 }
 
-
 app.post('/numbers/save', async (req, res) => {
-  const number = req.body
-  console.log(number);
-  const newNumber = await saveNumber(number);
-  res.status(201).send(newNumber);
-})
-
-app.get('/numbers/:id', async (req, res) => {
-  const calcId = req.params.id;
-  const result = await getNumbersForCalc(calcId);
-  if (result.length > 0) {
-    console.log(`result: ${result}`);
-    return res.status(200).send(result)
+  try {
+    const number = req.body
+    const newNumber = await saveOperand(number)
+    res.status(201).send(newNumber)
+  } catch (error) {
+    const errors = compileAndLogErrors(res, error)
+    res.status(500).json(errors)
   }
-
-  return res.status(404).send()
-
 })
+
+app.get('/numbers/:id', async (req, res, next) => {
+  try {
+    const calcId = req.params.id
+    const result = await getOperandsForCalculation(calcId)
+    if (result.length > 0) {
+      return res.status(200).send(result)
+    }
+
+    return res.status(404).send()
+  } catch (error) {
+    const errors = compileAndLogErrors(res, error)
+    res.status(500).json(errors)
+  }
+})
+
+app.use(function (err, req, res, next) {
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || 'error'
+
+  return res.status(err.statusCode).json({
+    status: err.status,
+    message: `Could not process the request: ${err.message}`
+  })
+})
+
+function compileAndLogErrors (res, error) {
+  const errors = []
+  if (error.name && error.name.toLowerCase().includes('sequelize')) {
+    error.errors.forEach(err => {
+      const flatError = {
+        name: err.name,
+        message: err.message
+      }
+      errors.push(flatError)
+    })
+  } else {
+    errors.push({
+      name: error.name,
+      message: error.message
+    })
+  }
+  logger.error({
+    time: new Date().toDateString(),
+    errors: errors
+  })
+  return errors
+}
